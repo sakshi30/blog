6d0249e6f9586f5105e91138543e3d4429325d60
Sakshi Srivastava <sakshi.srivastava@ai-fluence.com>
2025-08-20 22:42:47 +0530

    Updates

--- src/common/SectionTitle.js
+++
@@ -1,7 +0,0 @@
-const SectionTitle = ({ icon: Icon, title }) => (
-  <div className="flex items-center gap-2 mb-6 animate-fadeIn">
-    <Icon size={24} className="text-blue-500" />
-    <h2 className="text-2xl font-bold text-gray-800">{title}</h2>
-  </div>
-);
-export default SectionTitle;
